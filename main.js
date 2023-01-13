6eda60e8c2981a81dc5b851e1b47654301bbd644
Hlife97 <hidayet.hidayetov@gmail.com>
2023-01-14 00:25:59 +0400

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const joke = document.querySelector('#joke')
+const btn = document.querySelector('.btn')
+
+btn?.addEventListener('click', generateJoke)
+
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com', config)
+
+    const data = await res.json()
+
+    joke.innerHTML = data.joke
+}
